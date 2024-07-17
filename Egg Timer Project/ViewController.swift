cf4ff189294907830e8b55d72fe25218863e174c
razeiv07 <rajeevxrma@gmail.com>
2024-07-16 22:45:27 -0400

    Function to egg cooking time

--- Egg Timer Project/ViewController.swift
+++ Egg Timer Project/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimes : [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+//    let softTime = 5
+//    let mediumTime = 7
+//    let hardTime = 12
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+//        if hardness == "Soft" {
+//            print(softTime)
+//        } else if hardness == "Medium" {
+//            print(mediumTime)
+//        } else {
+//            print(hardTime)
+//        }
+        
+        switch hardness {
+        case "Soft":
+            print(eggTimes["Soft"])
+        case "Medium":
+            print(eggTimes["Medium"])
+        case "Hard":
+            print(eggTimes["Hard"])
+        default:
+            print("Error")
+        }
     }
-
-
 }
 
