95aaf093fe0c9e9edfed064053aafcc691632016
razeiv07 <rajeevxrma@gmail.com>
2024-07-17 22:43:15 -0400

    Title Label changes once the egg timer is completed

--- Egg Timer Project/ViewController.swift
+++ Egg Timer Project/ViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes : [String: Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes : [String: Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
     
     var secondsRemaining = 60
+    
+    var timer = Timer()
+    
 //    let softTime = 5
 //    let mediumTime = 7
 //    let hardTime = 12
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
         
         let hardness = sender.currentTitle!
         
@@ -44,13 +51,16 @@ class ViewController: UIViewController {
         //using optional binding to get the time which will decrease each second depending on the hardness level
         secondsRemaining = eggTimes[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0{
+        if secondsRemaining > 0 {
             print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE !... 😉 "
         }
     }
 }
