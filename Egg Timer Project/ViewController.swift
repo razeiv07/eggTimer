c4c1b5bd67c2e68f59f81fdf89f662a7ba7d0148
razeiv07 <rajeevxrma@gmail.com>
2024-07-17 23:13:04 -0400

    Added Progress Bar which shows the progress of egg depending on the hardness level

--- Egg Timer Project/ViewController.swift
+++ Egg Timer Project/ViewController.swift
@@ -10,18 +10,22 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes : [String: Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
-    
-    var secondsRemaining = 60
+    let eggTimes : [String: Int] = ["Soft": 3, "Medium": 4, "Hard": 7]
     
     var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
     
 //    let softTime = 5
 //    let mediumTime = 7
 //    let hardTime = 12
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        
         
         timer.invalidate()
         
@@ -49,15 +53,23 @@ class ViewController: UIViewController {
 //        }
         
         //using optional binding to get the time which will decrease each second depending on the hardness level
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed <= totalTime {
+            
+            let percentageaprogress = Float(secondsPassed) / Float(totalTime)
+            
+            progressBar.progress = Float(percentageaprogress)
+            
+            secondsPassed += 1
         } else {
             timer.invalidate()
             titleLabel.text = "DONE !... 😉 "
