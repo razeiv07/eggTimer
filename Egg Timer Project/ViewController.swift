e172021eca8af1d6ed5cb5fef60fb8433f867831
razeiv07 <rajeevxrma@gmail.com>
2024-07-18 21:48:19 -0400

    Alarm will beep when the egg is done cooking

--- Egg Timer Project/ViewController.swift
+++ Egg Timer Project/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -18,6 +19,8 @@ class ViewController: UIViewController {
     
     var totalTime = 0
     var secondsPassed = 0
+    
+    var player: AVAudioPlayer!
     
 //    let softTime = 5
 //    let mediumTime = 7
@@ -73,7 +76,14 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             titleLabel.text = "DONE !... 😉 "
+            playSound()
         }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
 
